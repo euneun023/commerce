6e40aa63de936d7f4f5068f32f556584ab35a5bc
euneun023 <euneun023@gamil.com>
2026-02-24 14:20:13 +0900

    fix: remove legacy manifests and clean up k8s directory

--- terraform/00-module/eks.tf
+++ terraform/00-module/eks.tf
@@ -77,5 +77,26 @@ module "lb_role" {
   }
 }
 
+resource "helm_release" "aws_load_balancer_controller" {
+  name = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart = "aws-load-balancer-controller"
+  namespace = "kube-system"
 
-
+  set {
+    name = "clusterName"
+    value = local.cluster_name
+  }
+  set {
+    name = "serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  set {
+    name = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.lb_role.iam_role_arn
+  }
+}
