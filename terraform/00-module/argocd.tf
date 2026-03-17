1c80d2f2efdcdb8e33f96eb2d7c94a3be696cb1c
dw2 <euneun023@gamil.com>
2026-03-18 01:42:00 +0900

    Add monitoring stack and ServiceMonitor via ArgoCD

--- terraform/00-module/argocd.tf
+++ terraform/00-module/argocd.tf
@@ -2,6 +2,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  version          = "9.4.11"
   namespace        = "argocd"
   create_namespace = true
 
